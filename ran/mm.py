01ffec6a5f4957418e7bbfec78970b888f1a1259
chenshenchao <chenshenchao@outlook.com>
2021-08-22 14:08:23 +0800

    绝对路径前缀。

--- ran/mm.py
+++ ran/mm.py
@@ -6,7 +6,7 @@ from inspect import getmodule
 from importlib import import_module, reload
 
 
-def list_module_mtime() -> dict:
+def list_module_mtime(prefix = None) -> dict:
     '''
     列举模块文件修改时间。
     '''
@@ -15,6 +15,8 @@ def list_module_mtime() -> dict:
     for n, m in sys.modules.items():
         if hasattr(m, '__file__'):
             p = getattr(m, '__file__')
+            if prefix != None and not p.startswith(prefix):
+                continue
             s = os.stat(p)
             result[n] = s.st_mtime_ns
     return result
@@ -38,14 +40,15 @@ class ModuleReloader:
     模块加载器。
     '''
 
-    def __init__(self):
+    def __init__(self, prefix=None):
         '''
         设置初始值。
         '''
 
         self.ticker = None
         self.able = True
-        self.mts = list_module_mtime()
+        self.prefix = prefix
+        self.mts = list_module_mtime(prefix)
 
     def watch(self, interval=1):
         '''
@@ -66,8 +69,10 @@ class ModuleReloader:
         重新加载模块。
         '''
 
-        nmts = list_module_mtime()
+        nmts = list_module_mtime(self.prefix)
         mmts = diff_module_mtime(nmts, self.mts)
+        self.mts = nmts
+
         # 重新加载模块
         for n in mmts.keys():
             if n == '__main__':
@@ -76,15 +81,15 @@ class ModuleReloader:
             reload(m)
         
         # 替换 from xx import xx 引入的变量。
-        if len(mmts) > 0:
-            for n in nmts.keys():
-                m = import_module(n)
-                for k, v in m.__dict__.items():
-                    if not hasattr(v, '__module__'):
-                        continue
-                    imn = getattr(v, '__module__')
-                    if imn in mmts:
-                        im = getmodule(v)
-                        nv = getattr(im, k)
-                        setattr(m, k, nv)
-        self.mts = nmts
+        if len(mmts) == 0:
+            return
+        for n in nmts.keys():
+            m = import_module(n)
+            for k, v in m.__dict__.items():
+                if not hasattr(v, '__module__'):
+                    continue
+                imn = getattr(v, '__module__')
+                if imn in mmts:
+                    im = getmodule(v)
+                    nv = getattr(im, k)
+                    setattr(m, k, nv)
